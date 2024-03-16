ab003fa4876d89b88f4c73f2519709d7c9278dd3
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-03-16 21:52:58 +0530

    Qrcode size edited

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -7,7 +7,7 @@ export const Qrcode = ({value}) => {
     size={256}
     // bgColor='#f4f1de'
     // fgColor='#3d405b'
-    style={{ height: "auto", maxWidth: "300px", width: "250px", border:'solid 2px #3d405b', borderRadius:'7px',padding:'5px' }}
+    style={{ height: "auto", maxWidth: "300px", width: "170px", border:'solid 2px #3d405b', borderRadius:'7px',padding:'5px' }}
     value={value}
     viewBox={`0 0 256 256`}
     />
