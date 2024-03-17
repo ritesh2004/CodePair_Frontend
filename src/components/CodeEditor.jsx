e5d8d364090904ad48bb67f247b5cf84530c094a
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-03-17 13:54:44 +0530

    monaco-editor integrated

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,23 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism.css'; //Example style, you can use another
+import React, { useEffect, useRef, useState } from 'react';
+// import Editor from 'react-simple-code-editor';
+// import { highlight, languages } from 'prismjs/components/prism-core';
+// import 'prismjs/components/prism-clike';
+// import 'prismjs/components/prism-javascript';
+// import 'prismjs/components/prism-python';
+// import 'prismjs/components/prism-sql';
+// import 'prismjs/components/prism-typescript';
+// import 'prismjs/components/prism-java';
+// import 'prismjs/components/prism-php';
+// import 'prismjs/components/prism-rust';
+// import 'prismjs/components/prism-csharp';
+// import 'prismjs/themes/prism.css'; //Example style, you can use another
+import Editor from '@monaco-editor/react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function CodeEditor({ id }) {
     const [code, setCode] = useState();
-    const [lang,setLang] = useState('languages.js');
+    const [lang,setLang] = useState('javascript');
+    const editorRef = useRef(null);
     const notify = (msg,type) => toast(msg,{position:'bottom-center',type:type,theme:'light'});
     useEffect(() => {
         const fetchCode = async () => {
             try {
                 const { data } = await axios({ method: 'GET', url: `https://code-pair-backend.vercel.app/api/v1/read/${id}` })
                 setCode(data?.texts);
-                setLang(`languages.${data?.lang}`);
+                setLang(data?.lang);
                 console.log(data.texts);
             } catch (error) {
                 notify('Something Went Wrong!','error');
@@ -26,21 +35,23 @@ function CodeEditor({ id }) {
         fetchCode();
     }, [])
 
+    function handleEditorDidMount(editor, monaco) {
+        editorRef.current = editor;
+    }
     return (
         <>
-            <div className='w-[90%] mx-auto my-5'>
+            <div className='w-[90%] mx-auto my-10'>
                 {code &&
-                    <div className="mockup-code">
-                        <pre>
+                    <div className="mockup-code h-[80vh]">
+                        <pre style={{paddingRight:'0'}}>
                             <Editor
-                                value={code}
-                                onValueChange={code => setCode(code)}
-                                highlight={code => highlight(code, lang)}
-                                padding={10}
-                                style={{
-                                    fontFamily: '"Fira code", "Fira Mono", monospace',
-                                    fontSize: 18,
-                                }}
+                                defaultValue={code}
+                                defaultLanguage='cpp'
+                                height = '80vh'
+                                width = '100%'
+                                theme='vs-dark'
+                                onMount={handleEditorDidMount}
+                                language={lang}
                             />
                         </pre>
                     </div>}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,22 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Editor from '@monaco-editor/react';
 
 export const Input = ({ data,setData }) => {
   const [code, setCode] = useState();
   const [lang, setLang] = useState();
   const [loading,setLoading] = useState(false);
+  const editorRef = useRef(null);
   const notify = (msg,type) => toast(msg,{position:'bottom-center',type:type,theme:'light'});
+  const handleEditorDidMount = (editor) => {
+    editorRef.current = editor;
+  }
+  const getCode = (value,event) => {
+    // console.log('Value',value);
+    setCode(value);
+  }
   const postCode = async () => {
     try {
       setLoading(true);
@@ -31,7 +40,7 @@ export const Input = ({ data,setData }) => {
       setData(data);
       notify('Code Uploaded!','success');
       setCode('');
-      setLang('js');
+      setLang('javascript');
     } catch (error) {
       setLoading(false);
       notify('Something went wrong!','error');
@@ -40,21 +49,35 @@ export const Input = ({ data,setData }) => {
   return (
     <div className='w-min flex flex-col justify-center items-center gap-3 md:w-[80%]'>
       <ToastContainer />
-      <textarea name='code' id='code' className="textarea textarea-info dark:text-white w-[320px] h-[300px] md:w-full md:h-[450px]" placeholder="Enter code you wanna share..." value={code} onChange={(e) => setCode(e.target.value)} ></textarea>
+      <div name='code' id='code' className="textarea textarea-info dark:text-white w-[320px] h-[300px] md:w-full md:h-[450px]" placeholder="Enter code you wanna share..." value={code} onChange={(e) => setCode(e.target.value)} >
+        <Editor
+          width='100%'
+          height='100%'
+          theme='vs-dark'
+          defaultLanguage='javascript'
+          defaultValue='// Type your code here...'
+          language={lang}
+          onChange={getCode}
+          onMount={handleEditorDidMount}
+        />
+      </div>
       <div className='w-full flex flex-col gap-2'>
       <label className='dark:text-white'>Select Language</label>
-      <select className="select select-info w-full dark:text-white" value={lang} onChange={(e) => setLang(e.target.value)}>
-        <option disabled defaultValue="js" value="js">Select Language</option>
-        <option value="js">JavaScript</option>
+      <select className="select select-info w-full dark:text-white">
+        <option disabled defaultValue="javascript" value="js">Select Language</option>
+        <option value="javascript">JavaScript</option>
         <option value="c">C</option>
         <option value="cpp">C++</option>
-        <option value="cs">C#</option>
-        <option value="py">Python</option>
-        <option value="jsvs">Java</option>
-        <option value="rs">Rust</option>
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+        <option value="rust">Rust</option>
         <option value="php">PHP</option>
         <option value="sql">SQL</option>
-        <option value="ts">TypeScript</option>
+        <option value="typescript">TypeScript</option>
+        <option value="html">HTML</option>
+        <option value="css">CSS</option>
+        <option value="json">JSON</option>
+        <option value="ruby">Ruby</option>
       </select>
       </div>
       <button className="btn rounded-3xl btn-info w-[320px] md:mb-5" disabled={loading} onClick={postCode}>{loading?<span className="loading loading-spinner"></span>:'Share Code'}</button>
